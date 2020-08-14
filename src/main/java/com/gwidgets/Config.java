dba59c305b1fc9a4c739ddc84f0eae202302a07c
otasyn <otasyn.github@gmail.com>
2020-08-14 00:31:29 -0400

    Initial download.

---
+++ src/main/java/com/gwidgets/Config.java
@@ -0,0 +1,31 @@
+package com.gwidgets;
+
+import javax.inject.Inject;
+import javax.ws.rs.ApplicationPath;
+
+
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
+import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
+
+@ApplicationPath("/")
+public class Config extends ResourceConfig {
+
+  @Inject
+  public Config(ServiceLocator serviceLocator) {
+    packages("com.gwidgets.resource");
+    Injector injector = Guice.createInjector(new GuiceModule());
+    initGuiceIntoHK2Bridge(serviceLocator, injector);
+  }
+
+
+  private void initGuiceIntoHK2Bridge(ServiceLocator serviceLocator, Injector injector) {
+    GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
+    GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
+    guiceBridge.bridgeGuiceInjector(injector);
+  }
+}
